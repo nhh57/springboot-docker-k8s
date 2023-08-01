4c159be0e1ba1175b5c66eb668d60aa2bbe79c06
hainh <nguyenhoanghai@vietbank.com.vn>
2023-08-01 14:49:14 +0700

    .

--- src/main/java/com/example/springboot/SpringbootDockerK8sApplication.java
+++ src/main/java/com/example/springboot/SpringbootDockerK8sApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringbootDockerK8sApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringbootDockerK8sApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootDockerK8sApplication.class, args);
+    }
 
 }
